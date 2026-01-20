e10963be60a0907d6d0dbf43e4dc6f8eae2ae8c2
Danny Griffin <dannygriffin@yahoo.com>
2026-01-20 10:53:28 +0000

    Initial Commit

---
+++ Clarity/ClarityApp.swift
@@ -0,0 +1,17 @@
+//
+//  ClarityApp.swift
+//  Clarity
+//
+//  Created by Dan Griffin on 20/01/2026.
+//
+
+import SwiftUI
+
+@main
+struct ClarityApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
